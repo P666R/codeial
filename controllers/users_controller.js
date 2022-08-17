354805adbb54ea85b8050fdd0207de8c26ef7391
PRoy <sam469roger@gmail.com>
2022-08-17 16:07:06 +0530

    implementing partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,8 @@
 module.exports.profile = function(req, res){
     // return res.end('<h1>user profile</h1>');
 
-    return res.render('users_profile',{
-        title: 'Users Profile'
+    return res.render('user_profile',{
+        title: 'User Profile'
     });
 }
 
