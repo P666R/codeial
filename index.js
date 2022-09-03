f1e82837daba29fd7522bd493bfe09e7404fd1fc
PRoy <sam469roger@gmail.com>
2022-09-04 05:09:17 +0530

    setting up and using scss

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const { urlencoded } = require('express');
 
+// used for session cookies
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo'); // removed (session)
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
 // reading through post request
 app.use(urlencoded({extended:false}));
 
@@ -21,12 +36,39 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
 
 // setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// mongo store is used to store the session
+app.use(session({
+    name: 'codeial',
+    // todo change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://127.0.0.1/codeial_development',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router ( shifted below to get express-session MW working)
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
