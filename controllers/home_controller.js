3e401a5a7f515c121017f8d2b20dce0fd446bcb0
PRoy <sam469roger@gmail.com>
2022-09-04 22:15:44 +0530

    display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,24 @@
-module.exports.home = function(req, res){
+const Post = require('../models/post');
+
+module.exports.home = function (req, res) {
     // return res.end('<h1>Express is up for codeial</h1>');
-
-    console.log(req.cookies);
-
+    // console.log(req.cookies);
     // change the value of cookie in the response
-    res.cookie('user_id', 25);
+    // res.cookie('user_id', 25);
 
-    return res.render('home',{
-        title: 'Home'
+    // Post.find({}, function(err, posts){
+    // return res.render('home',{
+    //     title: "Codeial | Home",
+    //     posts: posts
+    // });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function (err, posts) {
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
 }
 
